5390df45076b9aa8c962b5c1db0ef911a8ceed32
999999985 <advogadohenry@yahoo.com.br>
2021-12-23 13:07:13 -0300

    Add comandos do Git

--- Versão Exemplo.R
+++ Versão Exemplo.R
@@ -1,4 +1,22 @@
 #Versão 1
 #Versão 2
 #Versão 3 (Obs: Versões alteradas remotamente no site do Github)
-#Versão 4 
+#Versão 4
+
+$ git init (Inicializar o Git)
+$ git status (Verificar o status das atribuições)
+$ git add "Versão Exemplo.R"
+$ git commit -m "MENSAGEM: Versão de atualização dos comandos do Git"
+$ git push -u origin main (Para subir o arquivo comitado na branch "main")
+
+$ git pull (Para atualizar no Git e/ou no Github o que foi alterado de forma remota)
+
+$ git checkout -b "nova" (Para criar uma nova branch -> ex: "nova")
+$ git checkout main (Para trocar de brancho -> ex: Sair ir da branch "nova" e voltar para a branch "main")
+
+
+
+
+
+
+
