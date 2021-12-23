0eb92d9995a5a823d11b504f233c1804ec9aa113
999999985 <advogadohenry@yahoo.com.br>
2021-12-23 14:21:06 -0300

    MENSAGEM: Para mostra qual as branch existentes nos diretórios e qual a  branch esta selecionada (*)

--- Versão Exemplo.R
+++ Versão Exemplo.R
@@ -8,15 +8,7 @@ $ git status (Verificar o status das atribuições)
 $ git add "Versão Exemplo.R"
 $ git commit -m "MENSAGEM: Versão de atualização dos comandos do Git"
 $ git push -u origin main (Para subir o arquivo comitado na branch "main")
-
 $ git pull (Para atualizar no Git e/ou no Github o que foi alterado de forma remota)
-
 $ git checkout -b "nova" (Para criar uma nova branch -> ex: "nova")
 $ git checkout main (Para trocar de brancho -> ex: Sair da branch "nova" e voltar para a branch "main")
-
-
-
-
-
-
-
+$ git branch -a (Para mostra as branch existentes no diretórios e qual branch esta selecionada *)
