02f87efcd9c1ff48c340c8b9ccff58bcd34456cd
advogadohenry <93491772+999999985@users.noreply.github.com>
2021-12-23 12:28:21 -0300

    Atualização de Terceira Versão "Nova"
    Add no Git novos Valores (a=x, y=b) na branch "nova"

--- Versão Exemplo.R
+++ Versão Exemplo.R
@@ -1,13 +1,14 @@
 #Versão 1
 #Versão 2
 
-a <- 10
-b <- 20
+a <- 150
+b <- 3000
 
 c <- c(a*b)
 
 #Versão 3 - Na Branch "Nova"
 
 library(tidyverse)
+library (readxl)
 
-ggplot2::aes(a,b)
+ggplot2::ggplot() + geom_point(aes(x=a,y=b))
